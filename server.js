15794e8a9966ae0080025a686ed03078b0a73387
Keven Fortin-Simard <keven@hotelwizards.com>
2020-12-19 21:46:17 -0500

    Create & update server.js + Try to intregrate elements to app.js

--- server.js
+++ server.js
@@ -2,18 +2,53 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
+
+/* Dependencies*/
+const bodyParser = require('body-parser');
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8000;
+
+const server = app.listen(port, listening);
+
+function listening() {
+    console.log("server running");
+    console.log(`running on localhost: ${port}`);
+};
+
+// Initialize all route with a callback function
+
+// Callback function to complete GET '/all'
+
+// Post Route
+app.post('/addZip', addZip);
+
+function addZip(req, res) {
+
+    newEntry = {
+        zip: req.body.zip,
+        feelings: req.body.feelings
+    }
+
+    zipData.push(newEntry)
+    console.log(zipData)
+};
--- website/app.js
+++ website/app.js
@@ -2,4 +2,77 @@
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+// Personal API Key for OpenWeatherMap API
+/* de81d521f112510def4e1ed18f97247c*/
+
+// Event listener to add function to existing HTML DOM element
+
+/* Function called by event listener */
+
+/* Function to GET Web API Data*/
+let baseURL = 'api.openweathermap.org/data/2.5/weather?zip='
+let apiKey = '&appid=de81d521f112510def4e1ed18f97247c';
+
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e) {
+    const newZip = document.getElementById('zip').value;
+    getZip(baseURL, newZip, apiKey)
+
+}
+const getZip = async (baseURL, zip, key) => {
+
+    const res = await fetch(baseURL + zip + key)
+    try {
+
+        const data = await res.json();
+        console.log(data)
+        return data;
+    } catch (error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }
+}
+
+/* Function to POST data */
+postData('/addZip', {
+    zip: data.zip,
+    feelings: data.feelings
+});
+
+/* Function to GET Project Data */
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e) {
+    const newZip = document.getElementById('zip').value;
+    const newFeelings = document.getElementById('feelings').value;
+
+    getZip('/zipData', )
+        // New Syntax!
+        .then(function (data) {
+            // Add data
+            console.log(data);
+            postData('/addZip', {
+                zip: data.zip,
+                feelings: data.feelings
+            });
+        })
+        .then(
+            updateUI()
+        )
+}
+
+const updateUI = async () => {
+    const request = await fetch('/all');
+    try {
+        const allData = await request.json();
+        document.getElementById('entryDate').innerHTML = allData[0].date;
+        document.getElementById('zipTemp').innerHTML = allData[0].zip;
+        document.getElementById('entryContent').innerHTML = allData[0].feelings;
+
+    } catch (error) {
+        console.log("error", error);
+    }
+}
